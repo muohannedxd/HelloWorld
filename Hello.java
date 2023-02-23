1b23736f9a2707e2bced949ad72bf906a0f79000
linaahlem <90055322+linaahlem@users.noreply.github.com>
2023-02-23 16:22:13 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/**
+ * This class would print a hello world message by creating a class with a
+ * single print method
+ * @author Lina Ahlem Boualam
+ * @version 2023/02/23
+ */
+public class Hello {
+    /**
+     * The method prints to the console the message Hello World
+     * @retrun returns True when the message is printed successfully.
+     */
+    public boolean print(){
+        System.out.println("Hello World");
+        return true;
+    }
+    /**
+     * This is the main method to be invoked when launching the program
+     * @param args are the command line arguments
+     */
+    public static void main(String [] args){
+        Hello myHello= new Hello();
+        myHello.print();
+    }
+}
