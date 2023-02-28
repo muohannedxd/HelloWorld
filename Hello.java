7d5655413323afa00b73f2e4340ff216cfaaa007
muohannedxd <mohanned.kadache@ensia.edu.dz>
2023-02-28 12:42:40 +0100

    Added a very basic GUI

--- Hello.java
+++ Hello.java
@@ -1,3 +1,8 @@
+/*
+ * javax is a library that serves GUI in JAVA
+ */
+import javax.swing.*;
+
 /**
  * This class would print a hello world message by creating a class with a
  * single print method
@@ -13,6 +18,25 @@ public class Hello {
         System.out.println("Hello World");
         return true;
     }
+
+    /*
+     * function that creates the GUI
+     */
+    public void GUI() {
+        // creating the frame and label
+        JFrame frame = new JFrame("HELLO WORLD GUI");
+        JLabel label = new JLabel("Hello World", SwingConstants.CENTER);
+        
+        // adding the label to the frame
+        frame.add(label);
+
+        // adjusting the frame
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
     /**
      * This is the main method to be invoked when launching the program
      * @param args are the command line arguments
@@ -20,5 +44,9 @@ public class Hello {
     public static void main(String [] args){
         Hello myHello = new Hello();
         myHello.print();
+
+        // here, we will call the function that creates the GUI
+        Hello hh = new Hello();
+        hh.GUI();
     }
 }
